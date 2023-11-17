8997ed21de16f5c20e945686a985a91b18c9d7a0
Kacper Popinski <Kacpi5a5@o2.pl>
2023-11-17 22:53:09 +0100

    add media queries

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 {
-    const tasks = [
-        {
-            content: "naprawić rower",
-            done: false,
-        },
-        {
-            content: "wstać rano",
-            done: true,
-        },
-    ];
+    const tasks = [];
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
